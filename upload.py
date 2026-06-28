e101ec847e5a03efb20e473f61c20a7351ee652c
mwaseem603450 <mohammadw771@gmail.com>
2026-06-28 12:04:16 +0500

    Update upload.py

--- upload.py
+++ upload.py
@@ -9,7 +9,7 @@ ZIP_EXTS = [".zip", ".rar", ".7z", ".tar", ".gz"]
 MAX_SIZE_MB = 1990
 
 BOTS = [
-    {"token": "8916472332:AAGQRdFGjvgNyfsqqUDhaKvav4ZNG8S_kPk", "chat_id": 8586543259, "state_file": "state1.json"},
+    {"token": "8670254110:AAFlDPt1yxtACaEaZkGEgMFSzgBwfbNyj2Y", "chat_id": 8586543259, "state_file": "state1.json"},
 ]
 
 # ─── Helpers ────────────────────────────────────────────────────────────────
